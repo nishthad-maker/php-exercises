392099081da24670df4028a9256ec6d2b516616f
nishthad-maker <nishtha.dubey1024@gmail.com>
2026-05-22 21:15:05 -0400

    Fix HTML structure and improve form validation

--- section-3/while_loops.php
+++ section-3/while_loops.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">	
 	<title>While loops</title>	
 	
-	<meta name="description" content="while loops">
+	<meta name="description" content="While loops">
 	<meta name="author" content= "Nishtha Dubey">	
 
 	<!-- google fonts -->

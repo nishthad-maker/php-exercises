3f95d255cc1ae7c7c0e47a2c3aec9a9bc7c775a1
nishthad-maker <nishtha.dubey1024@gmail.com>
2026-05-22 20:32:53 -0400

    Update and rename activity_1.9.php to intramurals_form.php

--- section-3/intramurals_form.php
+++ section-3/intramurals_form.php
@@ -6,8 +6,8 @@
 	
 	<title>Sign UP</title>	
 	
-	<meta name="description" content="More about CSS">
-	<meta name="author" content="Author information goes here...">
+	<meta name="description" content="Form & user input">
+	<meta name="author" content="Nishtha Dubey">
 
 	<!-- link to font-awesome -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
